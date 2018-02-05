e1795ebfb7623a3337eb1292ef8b24e2fc6fcbc5
jswizzy <smithjustinn@hotmail.com>
2018-02-04 20:06:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/daggerexample/App.java
@@ -0,0 +1,22 @@
+package com.example.daggerexample;
+
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private ApplicationComponent mComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+    public ApplicationComponent getComponent() {
+        return mComponent;
+    }
+}
